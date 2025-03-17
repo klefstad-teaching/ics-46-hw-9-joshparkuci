1632b3ba397c68309bc4b85f639989f1ed0953f8
Joshua Park <joshuhp4@uci.edu>
2025-03-17 04:02:08 +0000

    ee

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -52,5 +52,5 @@ void print_path(const vector<int>& v, int total){
         cout << path << " ";
     }
     cout << endl;
-    cout << "Total cost: " << total << endl;
+    cout << "Total cost is " << total << endl;
 }
